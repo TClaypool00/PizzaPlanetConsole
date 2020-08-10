518996c8f14edfb34ba7216fe2bdaa60107e3693
claypooltyler00 <claypooltyler00@gmail.com>
2020-08-09 22:32:35 -0500

    Add a main menu and exit method

---
+++ PizzaPlanetConsole.App/Classes/Navigation.cs
@@ -0,0 +1,20 @@
+﻿using PizzaPlanetConsole2.DataAccess.DataModels;
+using System;
+
+namespace PizzaPlanetConsole.App.Classes
+{
+    public class Navigation
+    {
+        public static void MainMenu(Users user)
+        {
+            var store = StoreClass.PickAStore(user);
+            FoodClass.Order(store, user);
+        }
+
+        public static void Exit(int input)
+        {
+            if (input == 0)
+                Environment.Exit(0);
+        }
+    }
+}
