f8beea2e4c3473e3ad8143f82ae04697ab98429b
claypooltyler00 <claypooltyler00@gmail.com>
2020-08-15 22:33:24 -0500

    user can add inventories for store

---
+++ PizzaPlanetConsole.App/Classes/InventoryClass.cs
@@ -0,0 +1,50 @@
+﻿using PizzaPlanetConsole.DataAccess.DataModels;
+using System;
+
+namespace PizzaPlanetConsole.App.Classes
+{
+    public class InventoryClass
+    {
+        private static readonly ShoelessJoeContext ctx = new ShoelessJoeContext();
+
+        public static void AddInventory(Users user)
+        {
+            try
+            {
+                while (true)
+                {
+                    var inventory = new Inventory();
+
+                    Console.Write("Enter Store Id: ");
+                    inventory.StoreId = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+
+                    Console.Write("Enter Food Id: ");
+                    inventory.FoodId = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+
+                    Console.Write("Enter quanity: ");
+                    inventory.Quantity = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+
+                    ctx.Inventory.Add(inventory);
+                    ctx.SaveChanges();
+                    Console.WriteLine("Inventory added!");
+
+                    Console.Write("Would you like to add another? (y/n)");
+                    string again = Console.ReadLine();
+
+                    if (again.ToLower() != "y")
+                        break;
+                }
+                Navigation.PlaceOrder(user);
+            }
+            catch (Exception)
+            {
+                Console.Write("Something went wrong.");
+                GeneralClass.PressEnter();
+                Navigation.PlaceOrder(user);
+            }
+        }
+    }
+}
