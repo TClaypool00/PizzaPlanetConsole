4396bfcc9d7a7209cab74b5346ec4ea66c54dfec
claypooltyler00 <claypooltyler00@gmail.com>
2020-08-15 22:34:30 -0500

    made main menu more  easy to navigate through

--- PizzaPlanetConsole.App/Classes/Navigation.cs
+++ PizzaPlanetConsole.App/Classes/Navigation.cs
@@ -1,4 +1,4 @@
-﻿using PizzaPlanetConsole2.DataAccess.DataModels;
+﻿using PizzaPlanetConsole.DataAccess.DataModels;
 using System;
 
 namespace PizzaPlanetConsole.App.Classes
@@ -7,14 +7,71 @@ namespace PizzaPlanetConsole.App.Classes
     {
         public static void MainMenu(Users user)
         {
+            string[] options = { "Place Order", "Add Store", "Add Food", "My Orders" };
+
+            Console.WriteLine($"Welcome, {user.FirstName}!");
+            Console.WriteLine();
+
+            Display.DisplayOptions(options);
+            MenuOptions(user);
+        }
+
+        public static void MenuOptions(Users user)
+        {
+            try
+            {
+                int userInput = GeneralClass.UserPicks();
+
+                switch (userInput)
+                {
+                    case 1:
+                        PlaceOrder(user);
+                        break;
+                    case 2:
+                        StoreClass.AddStore(user);
+                        break;
+                    case 3:
+                        FoodClass.AddFood(user);
+                        break;
+                    case 4:
+                        int orderId = Display.DisplayList(user);
+                        OrderClass.OrderDetails(orderId, user);
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Not an option. Try again");
+                        MenuOptions(user);
+                        break;
+                }
+                GeneralClass.PressEnter();
+                MainMenu(user);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You must enter a number.");
+                MenuOptions(user);
+            }
+        }
+
+        public static void PlaceOrder(Users user)
+        {
             var store = StoreClass.PickAStore(user);
             FoodClass.Order(store, user);
         }
 
-        public static void Exit(int input)
+        
+
+        public static void BackToMainMenu(int input, Users user)
         {
-            if (input == 0)
-                Environment.Exit(0);
+            if (input == 999)
+                MainMenu(user);
+        }
+
+        public static void WriteBackMainMenu()
+        {
+            Console.WriteLine("Type '999' to go back to the main menu");
         }
     }
 }
