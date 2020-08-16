1b17cb9461400cbacf579bcb7b58345ba89594e4
claypooltyler00 <claypooltyler00@gmail.com>
2020-08-15 22:31:02 -0500

    can now add item to cart

---
+++ PizzaPlanetConsole.App/Classes/CartItemClass.cs
@@ -0,0 +1,28 @@
+﻿using PizzaPlanetConsole.DataAccess.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaPlanetConsole.App.Classes
+{
+    public class CartItemClass
+    {
+        private static readonly ShoelessJoeContext ctx = new ShoelessJoeContext();
+
+        public static void AddItem(ShoppingCart cart, int foodId)
+        {
+            Console.Write("How many do you want? ");
+            int num = int.Parse(Console.ReadLine());
+
+            var newItem = new CartItem
+            {
+                CartId = cart.CartId,
+                FoodId = foodId,
+                Quantity = num
+            };
+
+            ctx.CartItem.Add(newItem);
+            ctx.SaveChanges();
+        }
+    }
+}
